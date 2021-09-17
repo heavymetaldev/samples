3cc84047c0bb6c4840c864f8cf8678c62ba4a3cb
jakub.pawlowski <jakub.pawlowski@guestline.com>
2021-09-17 06:39:28 +0200

    split deploy.ps1 into functions

--- node-webapp/src/deploy.ps1
+++ node-webapp/src/deploy.ps1
@@ -1,139 +1,151 @@
-echo "Hello from DEPLOY.ps1!"
-write-output "otuput maybe?"
+echo "starting DEPLOY.ps1"
 
 # @if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
+
+function verifyPrerequisites() {
+  # :: Prerequisites
+  # :: -------------
+
+  # :: Verify node.js installed
+  if (!(get-command "node")) {
+    throw "Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment."
+  }
+}
 
 # :: ----------------------
 # :: KUDU Deployment Script
 # :: Version: 1.0.17
 # :: ----------------------
 
-# :: Prerequisites
-# :: -------------
+function setupVariables() {
 
-# :: Verify node.js installed
-if (!(get-command "node")) {
-  throw "Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment."
-}
+  # :: Setup
+  # :: -----
 
-# :: Setup
-# :: -----
+  # setlocal enabledelayedexpansion
 
-# setlocal enabledelayedexpansion
+  $env:ARTIFACTS = "($PSScriptRoot)\..\artifacts"
 
-# SET ARTIFACTS=%~dp0%..\artifacts
+  # see WellKnownEnvironmentVariables: https://github.com/projectkudu/kudu/blob/master/Kudu.Core/Deployment/WellKnownEnvironmentVariables.cs
+  if (!$env:DEPLOYMENT_SOURCE) { 
+    $env:DEPLOYMENT_SOURCE = $PSScriptRoot
+  }
 
-if (!$env:DEPLOYMENT_SOURCE) { 
-  $env:DEPLOYMENT_SOURCE = $PSScriptRoot
-}
+  if (!$env:DEPLOYMENT_TARGET) {
+    $env:DEPLOYMENT_TARGET = "$env:ARTIFACTS\wwwroot"
+  } 
 
-if (!$env:DEPLOYMENT_TARGET) {
-  $env:DEPLOYMENT_TARGET = "$env:ARTIFACTS\wwwroot"
-} 
+  if (!$env:NEXT_MANIFEST_PATH) {
+    $env:NEXT_MANIFEST_PATH = "$env:ARTIFACTS\manifest"
 
-if (!$env:NEXT_MANIFEST_PATH) {
-  $env:NEXT_MANIFEST_PATH = "$env:ARTIFACTS\manifest"
-
-  if (!$env:PREVIOUS_MANIFEST_PATH) {
-    $env:PREVIOUS_MANIFEST_PATH = "$env:ARTIFACTS\manifest"
+    if (!$env:PREVIOUS_MANIFEST_PATH) {
+      $env:PREVIOUS_MANIFEST_PATH = "$env:ARTIFACTS\manifest"
+    }
   }
 }
 
-if (!$env:KUDU_SYNC_CMD) {
-  #   :: Install kudu sync
-  echo "Installing Kudu Sync"
-  npm install kudusync -g --silent
-  if ($lastExitCode -ne 0) { throw "Failed to install KuduSync" }
+function selectNodeVersion() {
 
-  #   :: Locally just running "kuduSync" would also work
-  $env:KUDU_SYNC_CMD = "$env:appdata\npm\kuduSync.cmd"
-}
+  if ($env:KUDU_SELECT_NODE_VERSION_CMD) {
+    #   :: The following are done only on Windows Azure Websites environment
+    echo "trying to determine node version via '$env:KUDU_SELECT_NODE_VERSION_CMD'"
+    cmd /C $env:KUDU_SELECT_NODE_VERSION_CMD $env:DEPLOYMENT_SOURCE $env:DEPLOYMENT_TARGET $env:DEPLOYMENT_TEMP
+    if ($lastExitCode -ne 0) { throw "failed to select node version" }
 
-# :: Utility Functions
-# :: -----------------
-
-# :SelectNodeVersion
-
-if ($env:KUDU_SELECT_NODE_VERSION_CMD) {
-  #   :: The following are done only on Windows Azure Websites environment
-  echo "trying to determine node version via '$env:KUDU_SELECT_NODE_VERSION_CMD'"
-  cmd /C $env:KUDU_SELECT_NODE_VERSION_CMD $env:DEPLOYMENT_SOURCE $env:DEPLOYMENT_TARGET $env:DEPLOYMENT_TEMP
-  if ($lastExitCode -ne 0) { throw "failed to select node version" }
-
-  if (test-path "$env:DEPLOYMENT_TEMP\__nodeVersion.tmp") {
-    $env:NODE_EXE = cat "$env:DEPLOYMENT_TEMP\__nodeVersion.tmp"    
-  }
+    if (test-path "$env:DEPLOYMENT_TEMP\__nodeVersion.tmp") {
+      $env:NODE_EXE = cat "$env:DEPLOYMENT_TEMP\__nodeVersion.tmp"    
+      $env:PATH="$(split-path -parent $env:NODE_EXE);$env:PATH"
+    }
   
-  if (test-path "$env:DEPLOYMENT_TEMP\__npmVersion.tmp") {
-    $env:NPM_JS_PATH = cat "$env:DEPLOYMENT_TEMP\__npmVersion.tmp"
-  }
-
-    if (!$env:NODE_EXE) {
-      $env:NODE_EXE="node"
+    if (test-path "$env:DEPLOYMENT_TEMP\__npmVersion.tmp") {
+      $env:NPM_JS_PATH = cat "$env:DEPLOYMENT_TEMP\__npmVersion.tmp"
+      $npmBinPath = $env:NPM_JS_PATH.Replace("\node_modules\npm\bin\npm-cli.js", "")
+      $env:PATH="$npmBinPath;$env:PATH"
     }
 
-    $env:NPM_CMD="$env:NODE_EXE"
-    $env:NPM_ARGS="$env:NPM_JS_PATH"
-}
-else {
-  $env:NPM_CMD = "npm"
-  $env:NODE_EXE = "node"
-  $env:NPM_ARGS = ""
+    if (!$env:NODE_EXE) {
+      $env:NODE_EXE = "node"
+    }
+
+    $env:NPM_CMD = "$env:NODE_EXE $env:NPM_JS_PATH"
+  }
+  else {
+    $env:NPM_CMD = "npm"
+    $env:NODE_EXE = "node"
+  }
+
+  echo "NPM_CMD=$env:NPM_CMD"
+  echo "NODE_EXE=$env:NODE_EXE"
 }
 
-# goto :EOF
+function runKudSync($version = $null) {
+  if ($env:IN_PLACE_DEPLOYMENT -eq "1") {
+    echo "IN_PLACE_DEPLOYMENT enabled"
+    return
+  }
+  if (!$env:KUDU_SYNC_CMD -or $version -ne $null) {
+    #   :: Install kudu sync
+    if ($version -eq $null) { $version = "kudusync" }
+    echo "Installing Kudu Sync from $version"
+    npm install $version -g --silent
+    if ($lastExitCode -ne 0) { throw "Failed to install KuduSync" }
+
+    # $env:KUDU_SYNC_CMD = "kudusync"
+    $env:KUDU_SYNC_CMD = "$env:APPDATA\npm\kuduSync.cmd"
+    echo "KUDU_SYNC_CMD = $env:KUDU_SYNC_CMD"    
+  }
+  else {
+    echo "Found Kudu Sync at: $env:KUDU_SYNC_CMD"
+  }
+  
+  echo "$env:KUDU_SYNC_CMD -v 50 -f $env:DEPLOYMENT_SOURCE -t $env:DEPLOYMENT_TARGET -n $env:NEXT_MANIFEST_PATH -p $env:PREVIOUS_MANIFEST_PATH -i '.git;.hg;.deployment;deploy.cmd'"
+  & $env:KUDU_SYNC_CMD -v 50 -f $env:DEPLOYMENT_SOURCE -t $env:DEPLOYMENT_TARGET -n $env:NEXT_MANIFEST_PATH -p $env:PREVIOUS_MANIFEST_PATH -i ".git;.hg;.deployment;deploy.cmd"
+  if ($lastExitCode -ne 0) { throw "kuduSync failed." }  
+}
+
+function restore() {
+  # :: 3. Install npm packages
+  if (test-path "$env:DEPLOYMENT_TARGET\package.json") {
+    pushd $env:DEPLOYMENT_TARGET
+    try {
+      echo "restoring node_modules"
+      npm ci --production
+      if ($lastExitCode -ne 0) { throw "npm install failed." }
+    }
+    finally {
+      popd
+    }
+  }
+}
+
+function setAppOffline() {
+  echo "creating app_offline.htm"
+  echo "<html><body>App is currently offline.</body></html>" > "$env:DEPLOYMENT_TARGET/app_offline.htm"
+}
+
+function setAppOnline() {
+  echo "removing app_offline.htm"
+    
+  if (test-path "$env:DEPLOYMENT_TARGET/app_offline.htm") { 
+    rm "$env:DEPLOYMENT_TARGET/app_offline.htm"
+  }
+}
 
 # ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # :: Deployment
 # :: ----------
 
 echo "Handling node.js deployment."
+verifyPrerequisites
+setupVariables
+selectNodeVersion
 
-# :: 1. KuduSync
-if ($env:IN_PLACE_DEPLOYMENT -ne "1") {
-  & $env:KUDU_SYNC_CMD -v 50 -f $env:DEPLOYMENT_SOURCE -t $env:DEPLOYMENT_TARGET -n $env:NEXT_MANIFEST_PATH -p $env:PREVIOUS_MANIFEST_PATH -i ".git;.hg;.deployment;deploy.cmd"
-  if ($lastExitCode) { throw "kuduSync failed." }
-}
+setAppOffline
 
-# :: 2. Select node version
-# call :SelectNodeVersion
+runKudSync -version "qbikez/kudusync"
 
-# :: 3. Install npm packages
-if (test-path "$env:DEPLOYMENT_TARGET\package.json") {
-  pushd $env:DEPLOYMENT_TARGET
-  try {
-    echo "npm install: '$env:NPM_CMD'"
-    & $env:NPM_CMD $env:NPM_ARGS install --production
-    if ($lastExitCode -ne 0) { throw "npm install failed." }
-  }
-  finally {
-    popd
-  }
-}
+restore
 
-# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-# goto end
+setAppOnline
 
-# :: Execute command routine that will echo out when error
-# :ExecuteCmd
-# setlocal
-# set _CMD_=%*
-# call %_CMD_%
-# if "%ERRORLEVEL%" NEQ "0" echo Failed exitCode=%ERRORLEVEL%, command=%_CMD_%
-# exit /b %ERRORLEVEL%
-
-# :error
-# endlocal
-# echo An error has occurred during web site deployment.
-# call :exitSetErrorLevel
-# call :exitFromFunction 2>nul
-
-# :exitSetErrorLevel
-# exit /b 1
-
-# :exitFromFunction
-# ()
-
-# :end
-# endlocal
-# echo Finished successfully.
+echo "Finished successfully."
